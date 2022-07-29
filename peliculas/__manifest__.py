121246b435e2f1093557fdbe44f8f7eb3420936a
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-07-29 02:49:49 -0500

    asd

--- peliculas/__manifest__.py
+++ peliculas/__manifest__.py
@@ -8,5 +8,6 @@
     'description': """Este modulo le permite gestionar las peliculas que se alquilan en la tienda de videos de Ronaldinho Farfan""",
     'data': [
         'views/menu.xml',
+        'views/presupuesto.xml',
     ]
 }
--- peliculas/models/presupuesto.py
+++ peliculas/models/presupuesto.py
@@ -7,6 +7,8 @@ class Presupuesto(models.Model):
     _name = 'presupuesto'
     _description = 'Presupuesto de peliculas para alquiler'
     
+    
+    foto = fields.Binary(string="Foto")
     name = fields.Char(string='Pelicula', required=True)
     clasificacion = fields.Selection(selection=[('g','Publico General'),('PG','Mayores de 18 años'),('PG-13','Mayores de 13 años'),('R','Mayores de 17 años'),('NC-17','Mayores de 18 años'),('R','Se recomienda la compañia de un adulto')])
     fecha_estreno = fields.Date(string='Fecha de estreno')
@@ -15,4 +17,9 @@ class Presupuesto(models.Model):
     # apellido = fields.Char(string='Apellido', required=True)
     director_id = fields.Many2one('res.partner', string='Director', required=True)
     genero_ids = fields.Many2many('genero',string='Generos')
-    vista_general = fields.Text(string='Vista general')    
+    vista_general = fields.Text(string='Vista general')  
+    link_trailer = fields.Char(string='Link del trailer')
+    es_libro = fields.Boolean(string='Es libro', default=False)
+    libro = fields.Binary(string='Libro')
+    
+      

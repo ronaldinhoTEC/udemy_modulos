4a6a86cca9f337d957a580a82f40c218b399713d
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-07-29 00:21:52 -0500

    sad

--- peliculas/models/presupuesto.py
+++ peliculas/models/presupuesto.py
@@ -7,7 +7,8 @@ class Presupuesto(models.Model):
     _description = 'Presupuesto de peliculas para alquiler'
     
     name = fields.Char(string='Nombreee', required=True)
-    clasificacion = fields.Selection(selection=[('g','Publico General'),('PG','Mayores de 18 años'),('PG-13','Mayores de 13 años'),('R','Mayores de 17 años'),('NC-17','Mayores de 18 años'),('R','Se recomienda la compañia de un adulto')])
-    fecha_estreno = fields.Date(string='Fecha de estreno')
-    punctuacion = fields.Float(string='Puntuacion', digits=(2,1))
-    active = fields.Boolean(string='Activo', default=True)
+    # clasificacion = fields.Selection(selection=[('g','Publico General'),('PG','Mayores de 18 años'),('PG-13','Mayores de 13 años'),('R','Mayores de 17 años'),('NC-17','Mayores de 18 años'),('R','Se recomienda la compañia de un adulto')])
+    # fecha_estreno = fields.Date(string='Fecha de estreno')
+    # punctuacion = fields.Float(string='Puntuacion', digits=(2,1))
+    # active = fields.Boolean(string='Activo', default=True)
+    apellido = fields.Char(string='Apellido', required=True)

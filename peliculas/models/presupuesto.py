b393d41d99d0501edaa99ca83e14d88b5a7c5fad
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-07-29 02:25:17 -0500

    asd

--- peliculas/models/presupuesto.py
+++ peliculas/models/presupuesto.py
@@ -1,4 +1,5 @@
 # -*- coding utf-8 -*-
+import string
 from odoo import   models, fields, api
 
 class Presupuesto(models.Model):
@@ -6,13 +7,12 @@ class Presupuesto(models.Model):
     _name = 'presupuesto'
     _description = 'Presupuesto de peliculas para alquiler'
     
-    name = fields.Char(string='Nombreee', required=True)
+    name = fields.Char(string='Pelicula', required=True)
     clasificacion = fields.Selection(selection=[('g','Publico General'),('PG','Mayores de 18 años'),('PG-13','Mayores de 13 años'),('R','Mayores de 17 años'),('NC-17','Mayores de 18 años'),('R','Se recomienda la compañia de un adulto')])
     fecha_estreno = fields.Date(string='Fecha de estreno')
     punctuacion = fields.Float(string='Puntuacion', digits=(2,1))
     active = fields.Boolean(string='Activo', default=True)
     # apellido = fields.Char(string='Apellido', required=True)
-    director_id = fields.Many2one('res.partner')
-    genero_ids = fields.Many2many('genero')
-    
-    
+    director_id = fields.Many2one('res.partner', string='Director', required=True)
+    genero_ids = fields.Many2many('genero',string='Generos')
+    vista_general = fields.Text(string='Vista general')    

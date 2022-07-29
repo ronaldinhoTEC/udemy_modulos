65a2100a28ce8f6ff73db72df018fa4bf7cc5632
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-07-29 00:45:35 -0500

    asd

--- peliculas/__manifest__.py
+++ peliculas/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Peliculas',
     'version': '1.0',
-    'depends': ['base'],
+    'depends': ['contacts'],
     'author': 'Ronaldinho Farfan',
     'category': 'Peliculas',
     'description': """Este modulo le permite gestionar las peliculas que se alquilan en la tienda de videos de Ronaldinho Farfan""",

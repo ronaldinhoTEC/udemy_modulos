1cfbd99944b8ad7598732896df1ca28210bbea20
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-07-29 00:08:32 -0500

    w

--- peliculas/models/presupuesto.py
+++ peliculas/models/presupuesto.py
@@ -1,4 +1,5 @@
 # -*- coding utf-8 -*-
+from string import punctuation
 from unicodedata import name
 from odoo import   models, fields, api
 
@@ -8,4 +9,7 @@ class Presupuesto(models.Model):
     _description = 'Presupuesto de peliculas para alquiler'
     
     name = fields.Char(string='Nombre', required=True)
-
+    clasificacion = fields.Selection(selection=[('g','Publico General'),('PG','Mayores de 18 años'),('PG-13','Mayores de 13 años'),('R','Mayores de 17 años'),('NC-17','Mayores de 18 años'),('R','Se recomienda la compañia de un adulto')], string='Clasificacion', required=True)
+    fecha_estreno = fields.Date(string='Fecha de estreno')
+    punctuacion = fields.Float(string='Puntuacion', digits=(2,1))
+    active = fields.Boolean(string='Activo', default=True)

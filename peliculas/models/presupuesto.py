167acc4c545fd26e9799db4a6ecce06a8cfea1d5
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-07-30 02:35:19 -0500

    asd

--- peliculas/models/presupuesto.py
+++ peliculas/models/presupuesto.py
@@ -35,7 +35,7 @@ class Presupuesto(models.Model):
     def aprobar_presupuesto(self):
         logger.info('info:  goodsaso Presupuesto aprovado')
         logger.warning('warning: good Presupuesto aprovado')
-        logger.error('error: Presupuesto aprovado')
+        logger.error('error: yasta XDDDD  gaaal Presupuesto aprovado')
         
     
     def cancelar_presupuesto(self):

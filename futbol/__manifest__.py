9cfd000bb0e392633ff2cf4778bdd0d14eb0d574
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-07-31 04:42:58 -0500

    asd

---
+++ futbol/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Futbol',
+    'version': '1.0',
+    'depends': ['base','contacts'],
+    'author': 'Ronaldinho Farfan',
+    'category': 'Futbol',
+    'description': """Este modulo es para gestionar el una academia y los equipos de futbol en particular, los jugadores  y los partidos de futbol""",
+    'data': [
+        'views/menu.xml',
+        'views/equipo.xml',
+    ]
+}
